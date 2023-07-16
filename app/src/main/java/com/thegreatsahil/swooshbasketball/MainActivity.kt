1d2e78fc151cd2b681ec1c40d654c2acabb1014a
sahilkuthe <sahilkkuthe2004@gmail.com>
2023-07-16 20:28:13 +0530

    Change of screens done

--- app/src/main/java/com/thegreatsahil/swooshbasketball/MainActivity.kt
+++ app/src/main/java/com/thegreatsahil/swooshbasketball/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.thegreatsahil.swooshbasketball
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.thegreatsahil.swooshbasketball.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+private lateinit var binding: ActivityMainBinding
+
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        binding.getStartedBtn.setOnClickListener{
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
+
     }
+
+
+
 }
